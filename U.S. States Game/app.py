efe6a85000a924164478b4583908bf26024c1f1a
Danial Khakbaz <danialkhakbaz.dk@gmail.com>
2021-10-18 17:51:23 +0330

    U.S.StatesGame Refactor the code and Add a faster turtle

--- U.S. States Game/app.py
+++ U.S. States Game/app.py
@@ -1,41 +1,32 @@
-import pandas as pd
 from turtle import Turtle, Screen
+import pandas as pd
 
 turtle = Turtle()
 screen = Screen()
 
-answer_state = ""
 correct_answers = 0
 IMAGE = "blank_states_img.gif"
 
 data = pd.read_csv("50_states.csv")
-
-# Make the state names lowercase
 data["state"] = data["state"].str.lower()
+states = data["state"].to_list()
 
-# Make the state DataFrame to a list
-listed_states = data["state"].to_list()
+turtle.hideturtle()
+turtle.penup()
+turtle.speed(0)
 
-# Set the screen title
 screen.title("U.S. States Game")
-
-# Set the screen size
 screen.setup(width=0.53, height=0.65)
-
-# The Actuall Code for the game
 screen.bgpic(IMAGE)
-# turtle.shape(IMAGE)
 
 while correct_answers < 50:
     answer_state = screen.textinput(
         f"{correct_answers}/50 States Correct", "What's the state name?")
 
-    if answer_state in listed_states:
-        turtle.hideturtle()
-        turtle.penup()
-        
+    if answer_state in states:
+
         state = data[data["state"] == answer_state]
-        
+
         turtle.goto(int(state["x"]), int(state["y"]))
         turtle.write(answer_state)
 
